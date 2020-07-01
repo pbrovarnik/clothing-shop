2474ae96d1339af67e0c6385c1f0015c1c580abf
Pasha Brovarnik <pasha@Pashas-MacBook-Pro.local>
2020-07-01 14:47:06 -0700

    adding sign in and out page and form components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component.jsx';
+import SignInAndSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route path='/shop' component={ShopPage} />
+				<Route path='/signin' component={SignInAndSignOutPage} />
 			</Switch>
 		</div>
 	);
