481c121680b4748d78fcda74976fd4a2b979e47e
Pasha <pasha@getinkworks.com>
2024-06-20 23:15:16 -0700

    updating to current react and vite

--- client/src/components/with-spinner/with-spinner.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Spinner from '../spinner/spinner.conponent';
-
-const WithSpinner = (WrappedComponenet) => ({ isLoading, ...otherProps }) => {
-	return isLoading ? <Spinner /> : <WrappedComponenet {...otherProps} />;
-};
-
-export default WithSpinner;
